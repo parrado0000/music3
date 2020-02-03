8077bae03fa67e4392faea2de84b166fd498ca3e
alvaropatzi <alvarosaa772@gmail.com>
2020-02-03 09:50:50 -0400

    commit music2

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{ActivatedRoute} from '@angular/router';
 import{HeroesService} from '../../servicios/heroes.service';
+
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html'
